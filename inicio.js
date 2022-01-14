75b30c53fdd99cddc86cb55339b5c52463858bc1
StefBuitron <95386977+StefBuitron@users.noreply.github.com>
2022-01-13 23:13:50 -0300

    Add files via upload

---
+++ inicio.js
@@ -0,0 +1,15 @@
+function ir(){
+
+var s= 2428;
+var u=("ztef");
+if(document.form.password.value==s && document.form.login.value==u){
+alert("Bem vindo");
+window.location="Eu sou programador.html";}
+else{
+alert("Ingrese nome de usuario e senha corretos");
+}
+
+
+
+
+}
